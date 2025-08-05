431421bea0c7364d987876578eb3e3c37595abad
Hael <michelkleinth@gmail.com>
2025-08-05 13:28:08 -0300

    utilizando o casting

---
+++ src/casting.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let idade = 25;
+idade.toFixed();
+idade.length;
+idade.forEach(x => {
+});
+let nome = 35;
+//# sourceMappingURL=casting.js.map
---
+++ src/casting.ts
@@ -0,0 +1,8 @@
+let idade: any = 25;
+(idade as number).toFixed();
+(idade as string).length;
+(idade as string[]).forEach(x => {
+    
+})
+
+let nome: string = 35 as unknown as string
