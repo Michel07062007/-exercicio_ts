91cd49a4cf5116ce851115e1283db495604f99cc
Hael <michelkleinth@gmail.com>
2025-08-04 23:50:07 -0300

    aprendendo o basico do typescript

---
+++ src/tipos_basico.ts
@@ -0,0 +1,26 @@
+let estaChovendo: boolean = false;
+
+estaChovendo = true;
+
+let idade: number = 18;
+
+let altura: number = 1.60;
+
+const nacionalidade: string = 'brasileiro';
+
+const colegas: string[] = ['lucas','daniel'];
+
+const tecnologias: Array<string> = ['html','css','js','ts'];
+
+const notas: ReadonlyArray<number> = [5,6,4,9];
+
+const lista: [noem: string, estaEstudando: boolean, idade: number] = ['Michel', true, 18];
+
+let idadeDaAna: number | string = 25 
+idadeDaAna = '25 anos'
+
+let dadosDaApi: any;
+dadosDaApi = 10
+dadosDaApi = true
+dadosDaApi = [1,2,3]
+dadosDaApi = 'string'
