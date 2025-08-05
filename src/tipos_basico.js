c7ad86f361293ea8b122f174d44ad65dd7cedd07
Hael <michelkleinth@gmail.com>
2025-08-04 23:54:40 -0300

    compilando o ts

---
+++ src/tipos_basico.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let estaChovendo = false;
+estaChovendo = true;
+let idade = 18;
+let altura = 1.60;
+const nacionalidade = 'brasileiro';
+const colegas = ['lucas', 'daniel'];
+const tecnologias = ['html', 'css', 'js', 'ts'];
+const notas = [5, 6, 4, 9];
+const lista = ['Michel', true, 18];
+let idadeDaAna = 25;
+idadeDaAna = '25 anos';
+let dadosDaApi;
+dadosDaApi = 10;
+dadosDaApi = true;
+dadosDaApi = [1, 2, 3];
+dadosDaApi = 'string';
+//# sourceMappingURL=tipos_basico.js.map
